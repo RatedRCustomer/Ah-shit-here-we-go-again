45ed6854a8f2c7a19a66dc5b966301538ba70398
RatedRCustomer <i.denys.sychov@gmail.com>
2022-12-24 18:47:19 +0200

    Stage 2 Fix

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') #'django-insecure-!paugi0@dfq&yjmi$%)w=9e4)0olg&&fg%3^r2+y@j1w%eay%_'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
